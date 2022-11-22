f08f32bbbfb44feb1b557e084f1d8cb1ebc5136b
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-11-22 17:14:12 +0100

    creating main logic (node throws no error) and setting up Jest

--- calculateDueDate.js
+++ calculateDueDate.js
@@ -8,13 +8,11 @@ const finishingHour = 17;
 // isWorkDay + isWorkHour = isWorkTime
 
 function isWorkDay(date) {
-  return date.getUTCDay() >= 1 && date.getUTCDay() <= 5;
+  return date.getDay() >= 1 && date.getDay() <= 5;
 }
 
 function isWorkHour(date) {
-  return (
-    date.getUTCHours() >= startingHour && date.getUTCHours() <= finishingHour
-  );
+  return date.getHours() >= startingHour && date.getHours() <= finishingHour;
 }
 
 function isWorkTime(submitDate) {
@@ -32,16 +30,54 @@ function addDay(date) {
   date.setDate(date.getDate() + 1);
 }
 
+function addWeekDay(date, days) {
+  while (days) {
+    addDay(date);
+
+    if (isWorkDay(date)) {
+      --days;
+    }
+  }
+}
+
 // calcEstimate = start + TAT
+// if !sameDay
+
+function nextDay(date) {
+  addWeekDay(date, 1);
+  date.setHours(startingHour);
+  date.setMinutes(0);
+}
+
+function getRemainderTime(date) {
+  let day = new Date(date);
+  day.setHours(finishingHour);
+  day.setMinutes(0);
+
+  return day.getTime() - date.getTime();
+}
 
 // MAIN FUNCTION
 // CalculateDueDate(submitDate, TurnaroundTime, estimatedSolutionDate)
 
 function calculateDueDate(submitDate, turnaroundTime) {
   let estimation = new Date(submitDate);
+  let turnaroundMinutes = turnaroundTime * 60;
 
-  if (!isWorkTime(submitDate)) throw new Error('Cannot submit invalid date.');
+  if (!isWorkTime(submitDate) || isNaN(turnaroundTime) || turnaroundTime < 0)
+    throw new Error('Cannot submit: invalid request.');
 
+  while (turnaroundMinutes > 0) {
+    let remainderTime = getRemainderTime(estimate);
+    let workingTime = Math.min(remainderTime, turnaroundMinutes);
+
+    estimation.setTime(estimation.getTime() + workingTime);
+
+    turnaroundMinutes = turnaroundMinutes - workingTime;
+    if (turnaroundMinutes > 0) {
+      nextDay(estimation);
+    }
+  }
   return estimation;
 }
 
--- calculateDueDate.test.js
+++ calculateDueDate.test.js
@@ -1,11 +1,41 @@
-// TEST CASES
+const calculateDueDate = require('./calculateDueDate');
 
-// invalid (zero)
-// invalid (minus?)
-// invalid (early)
-// invalid (late)
-// invalid (weekend)
-// same day
-// next day (weekday)
-// next day (weekend)
-// next day (end of year)
+const testCases = [
+  // zero
+  // minus
+  // early
+  // late
+  // weekend
+  // same day
+  // next day - weekday
+  // next day - weekend
+  // next day - end of year
+];
+
+test('Submitted turnaround time cannot be zero.', () => {
+  expect(calculateDueDate(testCases[0])).toBe('');
+});
+test('Submitted turnaround time cannot be negative.', () => {
+  expect(calculateDueDate(testCases[1])).toBe('');
+});
+test('Submitted turnaround time is too early.', () => {
+  expect(calculateDueDate(testCases[2])).toBe('');
+});
+test('Submitted turnaround time is too late.', () => {
+  expect(calculateDueDate(testCases[3])).toBe('');
+});
+test('Submitted turnaround time has to end on a weekday.', () => {
+  expect(calculateDueDate(testCases[4])).toBe('');
+});
+test('Turnaround time will end same day.', () => {
+  expect(calculateDueDate(testCases[5])).toBe('');
+});
+test('Turnaround time will end next day.', () => {
+  expect(calculateDueDate(testCases[6])).toBe('');
+});
+test('Turnaround time will end next week.', () => {
+  expect(calculateDueDate(testCases[7])).toBe('');
+});
+test('Turnaround time will end next year.', () => {
+  expect(calculateDueDate(testCases[8])).toBe('');
+});
