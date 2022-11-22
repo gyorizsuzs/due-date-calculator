f7660558f834c9b6d56c47054c0e61869d364512
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-11-22 15:17:59 +0100

    limiting submitDate to working hours

--- calculateDueDate.js
+++ calculateDueDate.js
@@ -1,14 +1,48 @@
 // BASE LOGIC
 
-// MAIN FUNCTION
-// CalculateDueDate(submitDate, TurnaroundTime, estimatedSolutionDate)
-
 // DATA
 // workingHours = start(9), end(17)
-// workingDays = weekDay(+1), weekEnd(+3)
+const startingHour = 9;
+const finishingHour = 17;
+
+// isWorkDay + isWorkHour = isWorkTime
+
+function isWorkDay(date) {
+  return date.getUTCDay() >= 1 && date.getUTCDay() <= 5;
+}
+
+function isWorkHour(date) {
+  return (
+    date.getUTCHours() >= startingHour && date.getUTCHours() <= finishingHour
+  );
+}
+
+function isWorkTime(submitDate) {
+  return (
+    submitDate instanceof Date &&
+    typeof submitDate.getMonth === 'function' &&
+    isWorkDay(submitDate) &&
+    isWorkHour(submitDate)
+  );
+}
+
+// weekDay(+1)
+
+function addDay(date) {
+  date.setDate(date.getDate() + 1);
+}
 
 // calcEstimate = start + TAT
 
-// sameDay OR !sameDay
-// friday OR !friday
-// isWorkDay + isWorkHour = isWorkTime
+// MAIN FUNCTION
+// CalculateDueDate(submitDate, TurnaroundTime, estimatedSolutionDate)
+
+function calculateDueDate(submitDate, turnaroundTime) {
+  let estimation = new Date(submitDate);
+
+  if (!isWorkTime(submitDate)) throw new Error('Cannot submit invalid date.');
+
+  return estimation;
+}
+
+module.exports = calculateDueDate;
