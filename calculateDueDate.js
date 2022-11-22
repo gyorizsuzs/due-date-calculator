a8790c248b8d5bb16437f2906b8a4aa412c4f900
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-11-22 14:24:42 +0100

    creating the base of the project with logic and test cases set up

---
+++ calculateDueDate.js
@@ -0,0 +1,14 @@
+// BASE LOGIC
+
+// MAIN FUNCTION
+// CalculateDueDate(submitDate, TurnaroundTime, estimatedSolutionDate)
+
+// DATA
+// workingHours = start(9), end(17)
+// workingDays = weekDay(+1), weekEnd(+3)
+
+// calcEstimate = start + TAT
+
+// sameDay OR !sameDay
+// friday OR !friday
+// isWorkDay + isWorkHour = isWorkTime
---
+++ calculateDueDate.test.js
@@ -0,0 +1,11 @@
+// TEST CASES
+
+// invalid (zero)
+// invalid (minus?)
+// invalid (early)
+// invalid (late)
+// invalid (weekend)
+// same day
+// next day (weekday)
+// next day (weekend)
+// next day (end of year)
